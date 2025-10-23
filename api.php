b4fe6dade05414c5ccc113fe1ceda17335713092
safwanalabod2005-max <safwan.alabod2005@gmail.com>
2025-10-23 15:29:00 +0300

    api.php

---
+++ api.php
@@ -0,0 +1,27 @@
+<?php
+header('Content-Type: application/json');
+
+function parseSpecialChars($input) {
+    return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+}
+
+function decodeKeyFromUrl($key) {
+    return urldecode($key);
+}
+
+$validKey = parseSpecialChars("test");// That is your key
+$validIntegrityKey = parseSpecialChars("MrTusarRX");// that is integrityKey 
+
+
+$key = isset($_GET['key']) ? decodeKeyFromUrl(parseSpecialChars($_GET['key'])) : '';
+$integrityKey = isset($_GET['integrityKey']) ? parseSpecialChars($_GET['integrityKey']) : '';
+$response = array("Status" => "Failure", "MessageString" => "Invalid key or integrity key", "Username" => "");
+if ($key === $validKey && $integrityKey === $validIntegrityKey) {
+    $response["Status"] = "Success";
+    $response["MessageString"] = "Login successful!";
+    $response["Username"] = $key; 
+}
+
+// Return the response in JSON format
+echo json_encode($response);
+?>
